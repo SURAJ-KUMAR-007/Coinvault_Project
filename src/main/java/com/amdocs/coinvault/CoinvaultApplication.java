623ff40b28c18270c85b11ddff0d737dcf68cf23
SURAJ-KUMAR-007 <surajkumar9013078466@gmail.com>
2025-10-07 10:24:26 +0530

    First Commit

---
+++ src/main/java/com/amdocs/coinvault/CoinvaultApplication.java
@@ -0,0 +1,15 @@
+package com.amdocs.coinvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinvaultApplication.class, args);
+		System.out.print("FirstCOmmit");
+		
+	}
+
+}
---
+++ src/test/java/com/amdocs/coinvault/CoinvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs.coinvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
